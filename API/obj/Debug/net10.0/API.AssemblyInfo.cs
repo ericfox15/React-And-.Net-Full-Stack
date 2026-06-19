de2c88c5a09672c04dc17aa225e28f1130944a91
efox1515 <efox8@binghamton.edu>
2026-06-19 13:56:59 -0400

    git ignore

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fb1011d4e4a6a72cb57b4c589fd00af54abe7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192fa0d2908e1acb115468b4903978014f9f2b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
